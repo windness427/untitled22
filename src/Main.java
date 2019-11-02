a555d10bc51aa2dd3fbc73915631bb3bbc4e8adf
windness <windness427@gmail.com>
2019-11-02 16:40:11 +0900

    Initial commit 33

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+        import java.util.Queue;
+        import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
